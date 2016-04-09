dabbbfbc82aa95236187ffe4b131f5a7ea6acf0b
awanyogap <awanyogap@gmail.com>
2016-04-09 11:47:03 +0700

    Setting Up The Methods

--- app/src/main/java/com/example/root/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/root/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.root.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void displayForTeamA(int score)
+    {
+        TextView scoreTeamA = (TextView) findViewById(R.id.scoreTeamA);
+        scoreTeamA.setText(String.valueOf(score));
+    }
+
+    public void teamA3Points(View view)
+    {
+        displayForTeamA(3);
+    }
+
+    public void teamA2Points(View view)
+    {
+        displayForTeamA(2);
+    }
+
+    public void teamAFreeThrow(View view)
+    {
+        displayForTeamA(1);
     }
 }
